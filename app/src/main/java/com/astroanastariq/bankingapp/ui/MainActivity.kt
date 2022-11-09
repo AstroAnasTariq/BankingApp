2ee9243844a6731b50616f9ba3c31ed306e724a4
Anas Tariq <anasstarek73@gmail.com>
2022-11-09 22:47:19 +0200

    Added m3 AppBar

--- app/src/main/java/com/astroanastariq/bankingapp/ui/MainActivity.kt
+++ app/src/main/java/com/astroanastariq/bankingapp/ui/MainActivity.kt
@@ -2,9 +2,7 @@ package com.astroanastariq.bankingapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
-import com.astroanastariq.bankingapp.R
 import com.astroanastariq.bankingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +10,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.navHostFragmentContentMain
             .getFragment<NavHostFragment>().navController
+
+        setContentView(binding.root)
     }
 
     override fun onSupportNavigateUp(): Boolean {
