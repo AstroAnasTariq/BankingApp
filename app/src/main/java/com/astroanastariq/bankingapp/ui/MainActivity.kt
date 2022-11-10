cc1143fa881c33c317eaccba499edf67c2a4f072
Anas Tariq <anasstarek73@gmail.com>
2022-11-10 14:39:54 +0200

    Fixed AppBar

--- app/src/main/java/com/astroanastariq/bankingapp/ui/MainActivity.kt
+++ app/src/main/java/com/astroanastariq/bankingapp/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.astroanastariq.bankingapp.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.astroanastariq.bankingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        binding.navHostFragmentContentMain
+//        binding.navHostFragmentContentMain
+//            .getFragment<NavHostFragment>().navController
+        setSupportActionBar(binding.topAppBar)
+        //I've used .getFragment instead of findNavController
+        val navController = binding.navHostFragmentContentMain
             .getFragment<NavHostFragment>().navController
+
+        val config = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, config)
 
         setContentView(binding.root)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return binding.navHostFragmentContentMain
-            .getFragment<NavHostFragment>().navController
-            .navigateUp() || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        return binding.navHostFragmentContentMain
+//            .getFragment<NavHostFragment>().navController
+//            .navigateUp() || super.onSupportNavigateUp()
+//    }
 }
